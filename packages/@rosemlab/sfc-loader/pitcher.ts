d113e4d16c9ff2368aaa9781403ece04dcfcd48f
Roman Shevchenko <iroman.via@gmail.com>
2019-09-14 02:12:51 +0300

    [sfc-loader] Improved pitcher typing

--- packages/@rosemlab/sfc-loader/pitcher.ts
+++ packages/@rosemlab/sfc-loader/pitcher.ts
@@ -4,36 +4,46 @@ import LoaderContext = loader.LoaderContext
 import { getOptions, stringifyRequest } from 'loader-utils'
 import querystring, { ParsedUrlQuery } from 'querystring'
 import { SFC_LOADER_IDENT, SFCLoaderOptions } from '@rosemlab/sfc-loader/index'
-import { SFCLoaderPluginOptions } from './SFCLoaderPlugin'
+import scopedCSSLoader from '@rosemlab/scoped-css-loader'
+import {
+  getOptions as getPluginOptions,
+  SFCLoaderPluginOptions,
+} from './SFCLoaderPlugin'
+
+type ResolvedLoader = {
+  request: string
+  path: string
+  query: string
+  module: Function
+}
 
 const scopedCSSLoaderPath = require.resolve('@rosemlab/scoped-css-loader')
-const isPitcher = (loader: any): boolean => __filename === loader.path
-const isNullLoader = (loader: any): boolean =>
+const isPitcher = (loader: ResolvedLoader): boolean =>
+  __filename === loader.path
+const isNullLoader = (loader: ResolvedLoader): boolean =>
   /[/\\@]null-loader/.test(loader.path)
-const isCSSLoader = (loader: any): boolean =>
+const isCSSLoader = (loader: ResolvedLoader): boolean =>
   /[/\\@]css-loader/.test(loader.path)
-const isCacheLoader = (loader: any): boolean =>
+const isCacheLoader = (loader: ResolvedLoader): boolean =>
   /[/\\@]cache-loader/.test(loader.path)
 const sfcLoaderPath = require.resolve('./index')
-const isSFCLoader = (loader: any): boolean => loader.path === sfcLoaderPath
-const shouldIgnoreCustomBlock = (loaders: any[]) => {
-  const actualLoaders: any[] = loaders.filter((loader) => {
+const isSFCLoader = (loader: ResolvedLoader): boolean =>
+  loader.path === sfcLoaderPath
+const shouldIgnoreCustomBlock = (loaders: ResolvedLoader[]) => {
+  const actualLoaders: ResolvedLoader[] = loaders.filter((loader) => {
     return !isPitcher(loader) && !isSFCLoader(loader) && !isCacheLoader(loader)
   })
 
   return 0 === actualLoaders.length
 }
+const isStyleLang = (lang: string): boolean =>
+  /^((post)?c|le|s[a|c])ss|styl$/.test(lang)
 
 export default function(source: string): string {
   return source
 }
 
-export function pitch(
-  this: LoaderContext,
-  remainingRequest: any,
-  precedingRequest: any,
-  data: any
-): string | void {
+export function pitch(this: LoaderContext): string | void {
   // do not inject if user uses null-loader to void the type
   if (this.loaders.some(isNullLoader)) {
     return
@@ -48,7 +58,7 @@ export function pitch(
   // `.slice(1)` - remove "?" character
   const query: ParsedUrlQuery = querystring.parse(this.resourceQuery.slice(1))
   const options: SFCLoaderOptions = { ...(getOptions(this) || {}) }
-  // const pluginOptions: SFCLoaderPluginOptions = getOptions(this)
+  // const pluginOptions: SFCLoaderPluginOptions = getPluginOptions(this)
   // todo cache
   const { cacheDirectory, cacheIdentifier } = options
 
@@ -58,35 +68,34 @@ export function pitch(
   // Also make sure to dedupe based on loader path and query to be safe.
   // Assumes you'd probably never want to apply the same loader on the same
   // file twice.
-  const loaders: any[] = this.loaders
+  const loaders: ResolvedLoader[] = this.loaders
   const seen: { [identifier: string]: boolean } = {}
   const loaderStrings: string[] = []
 
   // Inject scoped-css-loader before css-loader for scoped CSS // todo trimming
   if (
     'style' === query.block ||
-    (query.lang && /^((post)?c|le|s[a|c])ss|styl$/.test(query.lang.toString()))
+    (query.lang && isStyleLang(query.lang.toString()))
   ) {
     const cssLoaderIndex: number = loaders.findIndex(isCSSLoader)
 
     if (cssLoaderIndex > -1) {
-      loaders.splice(
-        cssLoaderIndex + 1,
-        0,
+      loaders.splice(cssLoaderIndex + 1, 0, {
         // Use sfc-loader options in scoped-css-loader
-        `${scopedCSSLoaderPath}??${SFC_LOADER_IDENT}`
-      )
+        request: `${scopedCSSLoaderPath}??${SFC_LOADER_IDENT}`,
+        path: scopedCSSLoaderPath,
+        query: `??${SFC_LOADER_IDENT}`,
+        module: scopedCSSLoader,
+      })
     }
   }
 
-  loaders.forEach((loader: any) => {
+  loaders.forEach((loader: ResolvedLoader) => {
     if (isPitcher(loader)) {
       return
     }
 
-    const [identifier, request] = isString(loader)
-      ? [loader, loader]
-      : [loader.path + loader.query, loader.request]
+    const [identifier, request] = [loader.path + loader.query, loader.request]
 
     if (!seen[identifier]) {
       seen[identifier] = true
