7a7356ece571ace52f2ceb776569cdd7bfb41d7d
Roman Shevchenko <iroman.via@gmail.com>
2019-12-08 03:46:09 +0200

    [eslint-config-prettier] Improved basic ESLint config to handle newest ECMAScript features

--- packages/eslint-config-prettier/index.js
+++ packages/eslint-config-prettier/index.js
@@ -1,4 +1,14 @@
 module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   plugins: ['prettier'],
   extends: [
     'eslint:recommended',
